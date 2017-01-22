e829a4065443b547b770d1260c885817f1171a7a
DESKTOP-K48D6IA\Kuei <ragnakuei@livemail.tw>
2017-01-22 16:01:58 +0800

    新增 feature 檔與 step definition 檔

---
+++ CalculatorSampleTests/MyCalculatorSteps.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace CalculatorSampleTests
+{
+    [Binding]
+    public class MyCalculatorSteps
+    {
+        [Given(@"在第一個輸入項輸入 (.*)")]
+        public void Given在第一個輸入項輸入(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"在第二個輸入項輸入 (.*)")]
+        public void Given在第二個輸入項輸入(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"按下 Add 按鈕")]
+        public void When按下Add按鈕()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"螢幕上的結果應為 (.*)")]
+        public void Then螢幕上的結果應為(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
