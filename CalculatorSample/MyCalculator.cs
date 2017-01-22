d54f2af12e4244cf1c2acdbb1126adba01b59f42
DESKTOP-K48D6IA\Kuei <ragnakuei@livemail.tw>
2017-01-22 16:28:41 +0800

    測試案例寫完 > 第一個紅燈 完成

---
+++ CalculatorSample/MyCalculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CalculatorSample
+{
+    public class MyCalculator
+    {
+        public MyCalculator()
+        {
+        }
+
+        public int Add(int first, int second)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- CalculatorSampleTests/MyCalculatorSteps.cs
+++ CalculatorSampleTests/MyCalculatorSteps.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CalculatorSample;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using TechTalk.SpecFlow;
 
 namespace CalculatorSampleTests
@@ -6,28 +8,40 @@ namespace CalculatorSampleTests
     [Binding]
     public class MyCalculatorSteps
     {
+        private MyCalculator target;
+
+        [BeforeScenario]
+        private void BeforeScenario()
+        {
+            this.target = new MyCalculator();
+        }
+
         [Given(@"在第一個輸入項輸入 (.*)")]
-        public void Given在第一個輸入項輸入(int p0)
+        public void Given在第一個輸入項輸入(int first)
         {
-            ScenarioContext.Current.Pending();
+            ScenarioContext.Current.Set<int>(first, "first");
         }
-        
+
         [Given(@"在第二個輸入項輸入 (.*)")]
-        public void Given在第二個輸入項輸入(int p0)
+        public void Given在第二個輸入項輸入(int second)
         {
-            ScenarioContext.Current.Pending();
+            ScenarioContext.Current.Set<int>(second, "second");
         }
-        
+
         [When(@"按下 Add 按鈕")]
         public void When按下Add按鈕()
         {
-            ScenarioContext.Current.Pending();
+            var first = ScenarioContext.Current.Get<int>("first");
+            var second = ScenarioContext.Current.Get<int>("second");
+            int actual = target.Add(first, second);
+            ScenarioContext.Current.Set<int>(actual, "actual");
         }
-        
+
         [Then(@"螢幕上的結果應為 (.*)")]
-        public void Then螢幕上的結果應為(int p0)
+        public void Then螢幕上的結果應為(int expected)
         {
-            ScenarioContext.Current.Pending();
+            var actual = ScenarioContext.Current.Get<int>("actual");
+            Assert.AreEqual(expected, actual);
         }
     }
 }
